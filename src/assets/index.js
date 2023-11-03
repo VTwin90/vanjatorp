972615c95d43aa2206217e3d3c2b11fa6d99d8b3
VTwin90 <vanja.torp1@gmail.com>
2023-11-03 02:26:50 +0000

    init structure, template, img, layout, functions, design+

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import oslo from './backgrounds/oslo.jpg';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo.png';
+import logotext from './logo/logo_text.png';
+
+import vanja from './personal/vanja.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import figma from './tech/figma.png';
+
+
+import microverse from './company/microverse.png';
+
+import komikult from './projects/komikult.png';
+
+export {
+  bwmap,
+  oslo,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  vanja,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  figma,
+  komikult,
+  microverse,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,250 @@
+import {
+    frontend,
+    backend,
+    ux,
+    prototyping,
+    figma,
+    komikult,
+    microverse,
+  } from '../assets';
+  
+  export const navLinks = [
+    {
+      id: 'about',
+      title: 'About',
+    },
+    {
+      id: 'projects',
+      title: 'Projects',
+    },
+    {
+      id: 'contact',
+      title: 'Contact',
+    },
+  ];
+  
+  const services = [
+    {
+      title: 'Frontend Developer',
+      icon: frontend,
+    },
+    {
+      title: 'Backend Developer',
+      icon: backend,
+    },
+    {
+      title: 'UI/UX Design',
+      icon: ux,
+    },
+    {
+      title: 'Software Prototyping',
+      icon: prototyping,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+    {
+      name: 'figma',
+      icon: figma,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: 'Front-End Developer',
+      company_name: 'Microverse',
+      icon: microverse,
+      iconBg: '#333333',
+      date: 'Aug 2021 - Feb 2022',
+    },
+    {
+      title: 'Mentor (Volunteer)',
+      company_name: 'Microverse',
+      icon: microverse,
+      iconBg: '#333333',
+      date: 'Mar 2022 - May 2022',
+    },
+    {
+      title: 'Junior Software Engineer',
+      company_name: 'Microverse',
+      icon: microverse,
+      iconBg: '#333333',
+      date: 'May 2022 - Oct 2022',
+    },
+    {
+      title: 'Full Stack Developer',
+      company_name: 'Microverse',
+      icon: microverse,
+      iconBg: '#333333',
+      date: 'Sep 2022 - Present',
+    },
+  ];
+  
+  const projects = [
+    {
+      id: 'project-1',
+      name: 'KomiKult',
+      description: 'A comic characters list app that displays Marvel characters.',
+      tags: [
+        {
+          name: 'react',
+          color: 'blue-text-gradient',
+        },
+        {
+          name: 'mongodb',
+          color: 'green-text-gradient',
+        },
+        {
+          name: 'tailwind',
+          color: 'pink-text-gradient',
+        },
+      ],
+      image: komikult,
+      repo: 'https://github.com/',
+      demo: 'https://github.com/',
+    },
+    {
+      id: 'project-2',
+      name: 'KomiKult',
+      description:
+        'A comic characters list app that displays Marvel characters.',
+      tags: [
+        {
+          name: 'react',
+          color: 'blue-text-gradient',
+        },
+        {
+          name: 'restapi',
+          color: 'green-text-gradient',
+        },
+        {
+          name: 'scss',
+          color: 'pink-text-gradient',
+        },
+      ],
+      image: komikult,
+      repo: 'https://github.com/',
+      demo: 'https://github.com/',
+    },
+    {
+      id: 'project-3',
+      name: 'KomiKult',
+      description: 'A comic characters list app that displays Marvel characters.',
+      tags: [
+        {
+          name: 'nextjs',
+          color: 'blue-text-gradient',
+        },
+        {
+          name: 'supabase',
+          color: 'green-text-gradient',
+        },
+        {
+          name: 'css',
+          color: 'pink-text-gradient',
+        },
+      ],
+      image: komikult,
+      repo: 'https://github.com/',
+      demo: 'https://github.com/',
+    },
+    {
+      id: 'project-4',
+      name: 'KomiKult',
+      description: `A comic characters list app that displays Marvel characters.`,
+      tags: [
+        {
+          name: 'nextjs',
+          color: 'blue-text-gradient',
+        },
+        {
+          name: 'supabase',
+          color: 'green-text-gradient',
+        },
+        {
+          name: 'css',
+          color: 'pink-text-gradient',
+        },
+      ],
+      image: komikult,
+      repo: 'https://github.com/',
+      demo: 'https://github.com/',
+    },
+    {
+      id: 'project-5',
+      name: 'KomiKult',
+      description: 'A comic characters list app that displays Marvel characters.',
+      tags: [
+        {
+          name: 'nextjs',
+          color: 'blue-text-gradient',
+        },
+        {
+          name: 'supabase',
+          color: 'green-text-gradient',
+        },
+        {
+          name: 'css',
+          color: 'pink-text-gradient',
+        },
+      ],
+      image: komikult,
+      repo: 'https://github.com/',
+      demo: 'https://github.com/',
+    },
+  ];
+  
+  export { services, technologies, experiences, projects };
+  
