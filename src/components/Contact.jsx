c74e82bffe4f60753be8eebe887bd2136f3edebd
VTwin90 <vanja.torp1@gmail.com>
2023-11-08 16:19:02 +0000

    add email.js function and details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_bngz9em', // paste your ServiceID here (you'll get one when your service is created).
+        'template_bfblo5s', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Vanja Torp', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'vanja.torp1@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'm2uahbWG89_vJMVj9' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
