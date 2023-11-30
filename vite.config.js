1487be6d5ab49a8ba53b1e64d196ad75e639d7d1
Vanja Torp <56763771+VTwin90@users.noreply.github.com>
2023-11-30 22:33:45 +0000

    update vite.config.js with CommonJS syntax

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+const react = require('@vitejs/plugin-react');
 
 // https://vitejs.dev/config/
-export default defineConfig({
+module.exports = {
   plugins: [react()],
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
-});
+};
