79611eac93db099ba629b592ea8ba16a25603025
Vanja Torp <56763771+VTwin90@users.noreply.github.com>
2024-01-12 20:08:22 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-const react = require('@vitejs/plugin-react');
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-module.exports = {
+export default {
   plugins: [react()],
   esbuild: {
     jsxInject: `import React from 'react'`,
