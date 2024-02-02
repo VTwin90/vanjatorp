85bf88076d0aa314b6811df254e9f6fe7fa5010f
Vanja Torp <56763771+VTwin90@users.noreply.github.com>
2024-02-02 15:59:17 +0000

    add class justify-center to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -96,7 +96,7 @@ const Contact = () => {
   return (
     <div
       className="-mt-[8rem] xl:flex-row flex-col-reverse 
-      flex gap-10 overflow-hidden">
+      flex gap-10 overflow-hidden justify-center">
       <motion.div
         variants={slideIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] bg-jet p-8 rounded-2xl">
